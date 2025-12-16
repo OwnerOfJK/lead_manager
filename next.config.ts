601c6c910cd57562597564df3e9285f0d8b3c6e7
OwnerOfJK <john.kaller@hotmail.de>
2025-12-16 11:59:35 +0100

    Allow iframe embedding on personal website

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://ownerofjk.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
